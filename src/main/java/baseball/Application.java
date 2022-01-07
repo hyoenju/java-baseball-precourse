21eb6b3f5beb33aa2bf9a806f42d360465704436
hyoenju <l67378810@gmail.com>
2022-01-07 17:19:22 +0900

    Feat : 사용자 숫자 값 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,25 @@
 package baseball;
 
+import java.util.Scanner;
 import nextstep.utils.Randoms;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
         String randomNumber = generateRandomNumber();
+        String userInput = getUserInputNumber();
     }
 
     public static String generateRandomNumber() {
         int randomNumber = Randoms.pickNumberInRange(100, 999);
         return Integer.toString(randomNumber);
+    }
+
+    public static String getUserInputNumber() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("숫자를 입력해주세요 : ");
+        return sc.next();
     }
 }
