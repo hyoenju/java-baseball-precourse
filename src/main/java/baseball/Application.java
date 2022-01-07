5500806eebdb20d1f27f54419730a730f5128419
hyoenju <l67378810@gmail.com>
2022-01-07 17:36:48 +0900

    Feat : 사용자 입력값 조건 판단 기능 구현
    - isNumeric() : 숫자로 변환 가능한지 판단하는 함수
    - isThreeDigits() : 입력값이 3자리 숫자인지 판단하는 함수
    - isDuplicateNumbers() : 중복된 숫자가 없는지 판단하는 함수
    - isValidateNumber() : 위 세 조건을 모두 적용, strike/ball 계산 가능한 입력인지 판단하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,31 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import nextstep.utils.Randoms;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
         String randomNumber = generateRandomNumber();
         String userInput = getUserInputNumber();
+
+        while (!isValidateNumber(userInput)) {
+            System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
+            userInput = getUserInputNumber();
+        }
     }
 
     public static String generateRandomNumber() {
-        int randomNumber = Randoms.pickNumberInRange(100, 999);
+        int randomNumber;
+        while (true) {
+            randomNumber = Randoms.pickNumberInRange(100, 999);
+            if (isValidateNumber(Integer.toString(randomNumber))) {
+                break;
+            }
+        }
         return Integer.toString(randomNumber);
     }
 
@@ -21,5 +34,37 @@ public class Application {
 
         System.out.print("숫자를 입력해주세요 : ");
         return sc.next();
+    }
+
+    public static boolean isThreeDigits(String strNum) {
+        return strNum.length() == 3;
+    }
+
+    public static boolean isNumeric(String strNum) {
+        try {
+            Integer.parseInt(strNum);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isDuplicateNumbers(String[] numArr) {
+        Set<String> numSet = new HashSet<>(Arrays.asList(numArr));
+        String[] resultArr = numSet.toArray(new String[0]);
+
+        return resultArr.length == numArr.length;
+    }
+
+    public static boolean isValidateNumber(String strNum) {
+        if (!isNumeric(strNum)) {
+            return false;
+        } else if (!isThreeDigits(strNum)) {
+            return false;
+        } else if (!isDuplicateNumbers(strNum.split(""))) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
