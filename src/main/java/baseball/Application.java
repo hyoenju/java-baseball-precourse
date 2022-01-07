2867d1a89fc19e327f30f78dd251c3e4f5eef1dc
hyoenju <l67378810@gmail.com>
2022-01-07 17:16:15 +0900

    Feat : 랜덤숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        String randomNumber = generateRandomNumber();
+    }
+
+    public static String generateRandomNumber() {
+        int randomNumber = Randoms.pickNumberInRange(100, 999);
+        return Integer.toString(randomNumber);
     }
 }
